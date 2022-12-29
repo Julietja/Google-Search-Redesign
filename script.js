be23ea1cd3fba93f267aba7377820ae566670d08
Oluchi Alaribe <bamarachi6@gmail.com>
2022-12-29 04:20:24 +0100

    My first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+let butn = document.getElementById('mobile-butn')
+
+let dropdownMenu = document.getElementById('mobile-dropdown');
+
+function showMenu() {
+ dropdownMenu.style.display = "flex";
+ butn.style.display = "none";
+ 
+}
